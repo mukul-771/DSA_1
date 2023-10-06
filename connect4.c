033ab5f7ae87aa6e09f934023fd76eb552260c53
kailauday <kaila.reddy@iitgn.ac.in>
2023-10-06 23:33:46 +0530

    sim and connect4 are added

--- connect4.c
+++ connect4.c
@@ -1,33 +1,318 @@
 /* Implement connect 4 on a 5 (columns) x 4 (rows) board. */
-enum {
-    EMPTY = 0,
-    RED,
-    BLUE,
-};
+#include <stdio.h>
+#include <assert.h>
 
-typedef char board_t[4][5];
-typedef char player_t;
+#define n 4
+// Team members:SHAURY PATEL(22110241),MUKUL MEENA(22110159),KAILA UDAY(22110111)
+typedef char player_t; 
+typedef char board_t[4][5]; 
 
-int has_won(board_t board, player_t player)
-{}
+void init_board(board_t board)
+{
+    for (int row = 0; row < 4; ++row) {
+        for (int col = 0; col < 5; ++col) {
+            board[row][col] = '.';
+        }
+    }
+}
+
 
 int is_full(board_t board)
-{}
+{
+    for (int row = 0; row < 4; ++row) {
+        for (int col = 0; col < 5; ++col) {
+            if (board[row][col] == '.') { return 0; }
+        }
+    }
+    return 1;
+}
+
+int has_won(board_t board, player_t player)
+{
+    int cont=0;
+    for (int i=0;i<4;i++){
+        cont=0;
+        for (int j=0;j<4;j++){
+            if (board[i][j]==player){
+                cont++;
+            }
+        }
+        if (cont==n){
+            return 1;
+        }
+        cont=0;
+        for (int j=1;j<5;j++){
+            if (board[i][j]==player){
+                cont++;
+            }
+        }
+        if (cont==n){
+            return 1;
+        }
+    }
+    for (int j=0;j<5;j++){
+        cont=0;
+        for (int i=0;i<4;i++){
+            if (board[i][j]==player){
+                cont++;
+            }
+        }
+        if (cont==n){
+            return 1;
+        }
+    }
+    cont=0;
+    for (int i=0;i<4;i++){
+        if (board[i][i]==player){
+            cont++;
+        }
+    }
+    if (cont==n){
+        return 1;
+    }
+    cont=0;
+    for (int i=0;i<4;i++){
+        if (board[i][i+1]==player){
+            cont++;
+        }
+    }
+    if (cont==n){
+        return 1;
+    }
+    cont=0;
+    for (int i=0;i<4;i++){
+        if (board[i][n-i]==player){
+            cont++;
+        }
+    }
+    if (cont==n){
+        return 1;
+    }
+    cont=0;
+    for (int i=0;i<4;i++){
+        if (board[i][n-i-1]==player){
+            cont++;
+        }
+    }
+    if (cont==n){
+        return 1;
+    }
+    return 0;
+}
+
+player_t other_player(player_t player)
+{
+    switch (player) {
+    case 'r': return 'b';
+    case 'b': return 'r';
+    default: assert(0);
+    }
+}
 
 typedef struct {
     int col;
     int score;
 } move_t;
 
+void add_element(board_t board,player_t player, int j){
+    int i1=0;
+    for (int i=0;i<4;i++){
+        if (board[i][j]=='.'){
+            i1=i;
+        }
+    }
+    board[i1][j]=player;
+}
+
+void remove_element(board_t board, int j){
+    int i1=0;
+    for (int i=0;i<4;i++){
+        if (board[i][j]!='.'){
+            i1=i;
+            break;
+        }
+    }
+    board[i1][j]='.';
+}
+/*
+ * board should be an unfinished game.
+ */
 move_t best_move(board_t board, player_t player)
-{}
+{
+    move_t response;
+    move_t candidate;
+    int no_candidate = 1;
+
+    assert(!is_full(board));
+    assert(!has_won(board, player));
+    assert(!has_won(board, other_player(player)));
+
+    
+        for (int col = 0; col < 5; ++col) {
+            if (board[0][col] == '.') {
+                add_element(board,player,col);
+                if (has_won(board, player)) {
+                    remove_element(board,col);
+                    return (move_t) {
+                        col,1
+                    };
+                }
+                remove_element(board,col);
+            }
+        }
+
+    
+        for (int col = 0; col < 5; ++col) {
+            
+            if (board[0][col] == '.') {
+                add_element(board,player,col);
+                if (is_full(board)) {
+                    remove_element(board,col);
+                    return (move_t) {
+                        col,0
+                    };
+                }
+
+                response = best_move(board, other_player(player));
+                remove_element(board,col);
+                if (response.score == -1) {
+                    return (move_t) {
+                        .col=col,
+                        .score=1
+                    };
+                } else if (response.score == 0) {
+                    candidate = (move_t) {
+                        .col = col,
+                        .score = 0
+                    };
+                    no_candidate = 0;
+                } else { /* response.score == +1 */
+                    if (no_candidate) {
+                        candidate = (move_t) {
+                            .col = col,
+                            .score = -1
+                        };
+                        no_candidate = 0;
+                    }
+                }
+            }
+        }
+    
+    return candidate;
+}
 
 void print_board(board_t board)
-{}
+{
+    for(int i=0;i<4;i++){
+        for (int j=0;j<5;j++){
+            printf("%c ",board[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n----------------------------------\n");
+}
+
 
 int main()
 {
-    /* Your game play logic. */
-    /* The user should have the option to select red or blue player. */
+    board_t game;
+    init_board(game);
+    player_t player;
+    player_t AI;
+    player_t current;
+    int temp;
+    int column;
+    printf("Choose red or blue\n");
+    printf("Enter r for Red\n");
+    printf("Enter b for Blue\n");
+    scanf("%s",&temp);
+    if (temp==1){
+        player='r';
+        AI='b';
+    }
+    else{
+        player='b';
+        AI='r';
+    }
+    printf("For playing \n");
+    printf("For user enter 1\n");
+    printf("For computer enter 2\n");
+    scanf("%d",&temp);
+    if (temp==1){
+        current=player;
+    }
+    else{
+        current=AI;
+    }
+    if (current==player){
+    printf("Your turn (enter column 0-4): ");
+    scanf("%d", &column);
+    add_element(game, player, column);
+    if (column==0){
+        add_element(game,AI,1);
+    }
+    if (column==1){
+        add_element(game,AI,4);
+    }
+    if (column==2){
+        add_element(game,AI,4);
+  }
+    if (column==3){
+        add_element(game,AI,0);
+    }
+    if (column==4){
+        add_element(game,AI,3);
+    }
+    }
+    else{
+        add_element(game,AI,3);
+        current=player;
+    }
+    
+    while (1)
+    {
+        print_board(game);
+        if (player == current)
+        {
+            printf("Your turn (enter column 0-4): ");
+            scanf("%d", &column);
+            if (column < 0 || column > 4 || game[0][column] != '.')
+            {
+                printf("Invalid move. Try again.\n");
+                continue;
+            }
+            add_element(game, player, column);
+            if (has_won(game, player))
+            {
+                print_board(game);
+                printf("You win! Congratulations!\n");
+                break;
+            }
+        }
+        else
+        {
+            // Computer's turn
+            move_t computerMove = best_move(game, current);
+            printf("Computer's turn: %d\n", computerMove.col);
+
+            // Make the computer move
+            add_element(game, current, computerMove.col);
+
+            // Check if the computer has won
+            if (has_won(game, current))
+            {
+                print_board(game);
+                printf("Computer wins!\n");
+                break;
+            }
+        }
+        current = other_player(current);
+        if (is_full(game))
+        {
+            print_board(game);
+            printf("It's a draw!\n");
+        break;
+        }
+    }
     return 0;
 }
--- sim.c
+++ sim.c
@@ -1,40 +1,131 @@
+// Team members:SHAURY PATEL(22110241),MUKUL MEENA(22110159),KAILA UDAY(22110111)
 #include <stdio.h>
 #include <assert.h>
 
-#include "test.h"
-
-/*
- * The status of a line.
- */
 enum {
     NO = 0, /* No line */
     RED = 1,
     BLUE = 2
 };
 
-/*
- * The board records the colors of the lines.
- * board[0] = color of 12
- * board[1] = color of 13
- * ...
- * board[14] = color of 56
- */
 typedef char board_t[15];
 typedef char player_t; /* A player should be RED or BLUE. */
 
+void init_board(board_t board)
+{
+    for (int line = 0; line < 15; ++line) {
+        board[line] = NO;
+    }
+}
+
+player_t other_player(player_t player)
+{
+    switch (player) {
+    case RED: return BLUE;
+    case BLUE: return RED;
+    default: assert(0);
+    }
+}
+
 int has_won(board_t board, player_t player)
-{}
+{   
+    player_t otherplayer=other_player(player);
+    int triplets[20][3] = {
+        {0, 1, 5}, {0, 2, 6}, {0, 3, 7}, {0, 4, 8}, {1, 2, 9},
+        {1, 3, 10}, {1, 4, 11}, {2, 3, 12}, {2, 4, 13}, {3, 4, 14},
+        {5, 6, 9}, {5, 7, 10}, {5, 8, 11}, {6, 7, 12},
+        {6, 8, 13}, {7, 8, 14}, {9, 10, 12}, {9, 11, 13}, {10, 11, 14}, {12, 13, 14}
+    };
+
+    for (int i = 0; i < 20; i++) {
+        int a = triplets[i][0];
+        int b = triplets[i][1];
+        int c = triplets[i][2];
+        if (board[a] == otherplayer && board[b] == otherplayer && board[c] == otherplayer) {
+            return 1;
+        }
+    }
+
+   return 0;
+}
+
 
 int is_full(board_t board)
-{}
+{
+    for (int line = 0; line < 15; ++line) {
+        if (board[line] == NO){
+            return 0;
+        }
+    }
+    return 1;
+}
 
 typedef struct {
-    int line; /* 0 for 12, 1 for 13, ..., 14 for 56. */
-    int score; /* -1 for loss, 0 for draw, 1 for win. */
+    int line;
+    /* -1 for a loss, 0 for a draw, 1 for a win. */
+    int score;
 } move_t;
 
+/*
+ * board should be an unfinished game.
+ */
 move_t best_move(board_t board, player_t player)
-{}
+{
+    move_t response;
+    move_t candidate;
+    int no_candidate = 1;
+
+    for (int line = 0; line < 15; ++line) {
+        if (board[line] == NO) {
+            board[line] = player;
+            if (has_won(board, player)) {
+                board[line] = NO;
+                return (move_t) {
+                    line,
+                    1
+                };
+            }
+            board[line] = NO;
+        }
+    }
+
+    for (int line = 0; line < 15; ++line) {
+        if (board[line] == NO) {
+            board[line] = player;
+
+            response = best_move(board, other_player(player));
+            board[line] = NO;
+
+            if (is_full(board)) {
+                return (move_t) {
+                    .line = line,
+                    .score = 0
+                };
+            }
+            if (response.score == -1) {
+                return (move_t) {
+                    .line = line,
+                    .score = 1
+                };
+            } else if (response.score == 0) {
+                candidate = (move_t) {
+                    .line = line,
+                    .score = 0
+                };
+                no_candidate = 0;
+            } else { /* response.score == +1 */
+                if (no_candidate) {
+                    candidate = (move_t) {
+                        .line = line,
+                        .score = -1
+                    };
+                    no_candidate = 0;
+                }
+            }
+        }
+    }
+    return candidate;
+}
 
 void print_board(board_t board)
 {
@@ -42,13 +133,66 @@ void print_board(board_t board)
         switch (board[i]) {
         case RED: printf("R  "); break;
         case BLUE: printf("B  "); break;
+        case NO: printf(".  "); break;
         }
     }
+    printf("\n");
 }
 
 int main()
 {
-    /* Your game play logic. */
-    /* The user should have the option to select red or blue player. */
+    int move, i;
+    board_t board;
+    move_t response;
+    int player_choice;
+    printf("First player or second player (1 or 2): ");
+    scanf("%d", &player_choice);
+    player_t current;
+
+    if (player_choice == 1) {
+        current = RED;
+    } else if (player_choice == 2) {
+        current = BLUE;
+    } else {
+        printf("\nInvalid choice");
+        return 1; // Exit the program due to an invalid choice
+    }
+
+    init_board(board);
+
+    while (1) {
+        print_board(board);
+        printf("\n\n");
+
+        if (current == RED) {
+            printf("0  1  2  3  4  5  6  7  8  9 10 11 12 13 14\n");
+            printf("\nEnter your move: ");
+            scanf("%d", &move);
+            i = move;
+            if (i >= 0 && i < 15 && board[i] == NO) {
+                board[i] = current;
+            } 
+            else {
+                printf("Invalid move. Try again.\n");
+                continue;
+            }
+        } else {
+            response = best_move(board, current);
+            board[response.line] = current;
+        }
+
+        if (has_won(board, current)) {
+            print_board(board);
+            printf("Player %c has won!\n", (current == RED) ? 'R' : 'B');
+            break;
+        } else if (is_full(board)) {
+            print_board(board);
+            printf("It's a draw!\n");
+            break;
+        }
+
+        current = other_player(current);
+    }
+
     return 0;
 }

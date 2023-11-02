fe0fb54f4648c57a9063487550f67a1447cabf59
kailauday <kaila.reddy@iitgn.ac.in>
2023-11-02 22:44:10 +0530

    sim is updated

--- sim.c
+++ sim.c
@@ -3,57 +3,84 @@
 #include <assert.h>
 
 enum {
-    NO = 0, /* No line */
+    NO = 0,
     RED = 1,
     BLUE = 2
 };
 
 typedef char board_t[15];
-typedef char player_t; /* A player should be RED or BLUE. */
+typedef char player_t;
 
 void init_board(board_t board)
 {
-    for (int line = 0; line < 15; ++line) {
-        board[line] = NO;
+    for (int i = 0; i < 15; ++i) {
+        board[i] = NO;
     }
 }
 
 player_t other_player(player_t player)
 {
     switch (player) {
-    case RED: return BLUE;
-    case BLUE: return RED;
-    default: assert(0);
+        case RED: return BLUE;
+        case BLUE: return RED;
+        default: assert(0);
     }
 }
 
 int has_won(board_t board, player_t player)
-{   
-    player_t otherplayer=other_player(player);
-    int triplets[20][3] = {
-        {0, 1, 5}, {0, 2, 6}, {0, 3, 7}, {0, 4, 8}, {1, 2, 9},
-        {1, 3, 10}, {1, 4, 11}, {2, 3, 12}, {2, 4, 13}, {3, 4, 14},
-        {5, 6, 9}, {5, 7, 10}, {5, 8, 11}, {6, 7, 12},
-        {6, 8, 13}, {7, 8, 14}, {9, 10, 12}, {9, 11, 13}, {10, 11, 14}, {12, 13, 14}
-    };
-
-    for (int i = 0; i < 20; i++) {
-        int a = triplets[i][0];
-        int b = triplets[i][1];
-        int c = triplets[i][2];
-        if (board[a] == otherplayer && board[b] == otherplayer && board[c] == otherplayer) {
-            return 1;
+{
+    int a[4] = {4, 7, 9, 10};
+    int b[3] = {3, 5, 6};
+    int c[2] = {2, 3};
+    int d = 1;
+    
+    for (int i = 0; i < 4; i++) {
+        for (int j = i + 1; j <= 4; j++) {
+            if (board[i] == other_player(player) &&
+                board[j] == other_player(player) &&
+                board[j + a[i]] == other_player(player)) {
+                return 1;
+            }
         }
     }
 
-   return 0;
+    for (int i = 5; i < 8; i++) {
+        for (int j = i + 1; j <= 8; j++) {
+            if (board[i] == other_player(player) &&
+                board[j] == other_player(player) &&
+                board[j + b[i - 5]] == other_player(player)) {
+                return 1;
+            }
+        }
+    }
+
+    for (int i = 9; i < 11; i++) {
+        for (int j = i + 1; j <= 11; j++) {
+            if (board[i] == other_player(player) &&
+                board[j] == other_player(player) &&
+                board[j + c[i - 9]] == other_player(player)) {
+                return 1;
+            }
+        }
+    }
+
+    for (int i = 12; i < 13; i++) {
+        for (int j = i + 1; j <= 13; j++) {
+            if (board[i] == other_player(player) &&
+                board[j] == other_player(player) &&
+                board[j + d] == other_player(player)) {
+                return 1;
+            }
+        }
+    }
+
+    return 0;
 }
 
-
-int is_full(board_t board)
+int is_full(board_t board) 
 {
-    for (int line = 0; line < 15; ++line) {
-        if (board[line] == NO){
+    for (int i = 0; i < 15; i++) {
+        if (board[i] == NO) {
             return 0;
         }
     }
@@ -62,68 +89,51 @@ int is_full(board_t board)
 
 typedef struct {
     int line;
-    /* -1 for a loss, 0 for a draw, 1 for a win. */
     int score;
 } move_t;
 
-/*
- * board should be an unfinished game.
- */
 move_t best_move(board_t board, player_t player)
 {
     move_t response;
     move_t candidate;
     int no_candidate = 1;
 
-    for (int line = 0; line < 15; ++line) {
-        if (board[line] == NO) {
-            board[line] = player;
+    //Base case
+    for (int i = 0; i < 15; ++i) {
+        if (board[i] == NO) {
+            board[i] = player;
             if (has_won(board, player)) {
-                board[line] = NO;
-                return (move_t) {
-                    line,
-                    1
-                };
+                board[i] = NO;
+                return (move_t){i, 1};
             }
-            board[line] = NO;
+            board[i] = NO;
         }
     }
 
-    for (int line = 0; line < 15; ++line) {
-        if (board[line] == NO) {
-            board[line] = player;
-
+    //Predictions
+    for (int i = 0; i < 15; ++i) {
+        if (board[i] == NO) {
+            board[i] = player;
             response = best_move(board, other_player(player));
-            board[line] = NO;
-
+            board[i] = NO;
             if (is_full(board)) {
-                return (move_t) {
-                    .line = line,
-                    .score = 0
-                };
+                board[i] = NO;
+                return (move_t) {i, 0};
             }
             if (response.score == -1) {
-                return (move_t) {
-                    .line = line,
-                    .score = 1
-                };
+                return (move_t){i, 1};
             } else if (response.score == 0) {
-                candidate = (move_t) {
-                    .line = line,
-                    .score = 0
-                };
+                candidate = (move_t){i, 0};
                 no_candidate = 0;
-            } else { /* response.score == +1 */
+            } else {
                 if (no_candidate) {
-                    candidate = (move_t) {
-                        .line = line,
-                        .score = -1
-                    };
+                    candidate = (move_t){i, -1};
                     no_candidate = 0;
                 }
             }
         }
     }
+
     return candidate;
 }
 
@@ -131,9 +141,9 @@ void print_board(board_t board)
 {
     for (int i = 0; i < 15; ++i) {
         switch (board[i]) {
-        case RED: printf("R  "); break;
-        case BLUE: printf("B  "); break;
-        case NO: printf(".  "); break;
+            case NO: printf("0  "); break;
+            case RED: printf("R  "); break;
+            case BLUE: printf("B  "); break;
         }
     }
     printf("\n");
@@ -144,18 +154,18 @@ int main()
     int move, i;
     board_t board;
     move_t response;
-    int player_choice;
-    printf("First player or second player (1 or 2): ");
-    scanf("%d", &player_choice);
+    int x;
+    printf("You are red colour, Do you choose to be player 1 or 2: ");
+    scanf("%d", &x);
     player_t current;
 
-    if (player_choice == 1) {
+    if (x == 1) {
         current = RED;
-    } else if (player_choice == 2) {
+    } else if (x == 2) {
         current = BLUE;
     } else {
         printf("\nInvalid choice");
-        return 1; // Exit the program due to an invalid choice
+        return 1; // Exit the program due to invalid choice
     }
 
     init_board(board);
@@ -184,10 +194,6 @@ int main()
         if (has_won(board, current)) {
             print_board(board);
             printf("Player %c has won!\n", (current == RED) ? 'R' : 'B');
-            break;
-        } else if (is_full(board)) {
-            print_board(board);
-            printf("It's a draw!\n");
             break;
         }
 
